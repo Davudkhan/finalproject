073c9a7033fb2151338eeda18907219b2b3acefd
Davudkhan <davudkhan3333@gmail.com>
2022-03-25 22:22:43 +0400

    Navbaryaradildi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
-import './App.css';
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+//komponentler gelecek
+
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <h1>Salam</h1>
+   <div className='container-fluid'>
+    <Navbar/>
+    </div>
   );
 }
 
