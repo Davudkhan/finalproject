581c9c0f482a8c79eac7b6f85688fe0240ad5856
Davudkhan <davudkhan3333@gmail.com>
2022-05-27 21:08:18 +0400

    Navbar yaradildi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import logo from '../logooo.jpg'
+import logo from '../122222.PNG'
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-white">
   <a className="navbar-brand" href="#"><img src={logo} className="logo" alt="" /></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
